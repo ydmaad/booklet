ca758705e95f4467858fb192f8477ecff26089c3
Minae <ydmaad@gmail.com>
2025-10-04 21:37:46 +0900

    chore: 초기 필요 컴포넌트 생성

---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import MyReviewList from "../components/MyReviewList";
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header />
+      <MyReviewList />
+      <Footer />
+    </div>
+  );
+};
+
+export default MainPage;
