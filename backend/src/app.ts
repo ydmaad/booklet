ec3557afe09c463dcdc8022e8880ca85d9883dc2
Minae <ydmaad@gmail.com>
2025-10-04 22:54:59 +0900

    chore: 서버 기본 코드 추가

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,2 +1,19 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "서버 작동중!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`서버 실행: http://localhost:${PORT}`);
+});
