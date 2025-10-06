e34067b9ef7a7ab416785d2080f19ba3f789798f
Minae <ydmaad@gmail.com>
2025-10-06 22:16:13 +0900

    chore: 전체 패딩 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import MainPage from "./pages/mainPage";
 import BarcodeScanPage from "./pages/BarcodeScanPage";
-import MyPage from "./pages/myPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import MainPage from "./pages/MainPage";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/barcode" element={<BarcodeScanPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-    </Routes>
+    <div className="px-32">
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/barcode" element={<BarcodeScanPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </div>
   );
 }
 
