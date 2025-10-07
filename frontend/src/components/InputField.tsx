ea6b405cabcca23347c606a727212b0ddb6465f0
Minae <ydmaad@gmail.com>
2025-10-07 21:39:04 +0900

    feat: 북리뷰 input -> 컴포넌트화

---
+++ frontend/src/components/InputField.tsx
@@ -0,0 +1,28 @@
+interface InputFieldProps {
+  label: string;
+  value?: string;
+  placeholder: string;
+  onChange?: (val: string) => void;
+}
+
+const InputField = ({
+  label,
+  value,
+  placeholder,
+  onChange,
+}: InputFieldProps) => {
+  return (
+    <div className="flex items-center gap-20 mb-5">
+      <p className="text-lg w-[120px]">{label}</p>
+      <input
+        type="text"
+        value={value}
+        placeholder={placeholder}
+        onChange={(e) => onChange?.(e.target.value)}
+        className="flex-1 text-lg border py-1 px-2 rounded-lg"
+      />
+    </div>
+  );
+};
+
+export default InputField;
