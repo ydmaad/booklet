2f8101254d262548524236a729e8070003eb226e
thwjd639 <thwjd639@gmail.com>
2025-10-07 20:25:00 +0900

    feat: Redux 스토어 및 타입 안전 hooks 구현

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./slices/booksSlice";
-import authReducer from './authSlice';
+import authReducer from './slices/authSlice';
 
 export const store = configureStore({
   reducer: {
