126969c8b184faa61dbf2d6a1574cd03b6c7cfd2
Minae <ydmaad@gmail.com>
2025-10-07 18:46:46 +0900

    feat: 입력 받은 isbn 번호로 책 검색하여 데이터 페칭 구현

--- frontend/src/pages/MyReviewPage.tsx
+++ frontend/src/pages/MyReviewPage.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 const MyReviewPage = () => {
-  return <div>MyReviewPage</div>;
+  const { isbn } = useParams();
+  const [bookInfo, setBookInfo] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isbn) {
+      navigate("/isbn");
+      return;
+    }
+
+    const fetchBookInfo = async () => {
+      try {
+        setLoading(true);
+        setError("");
+
+        const response = await fetch(
+          `http://localhost:3000/api/books/isbn/${isbn}`
+        );
+
+        const data = await response.json();
+        console.log("받아온 책 정보:::", data.item[0]);
+      } catch (err: any) {
+        console.error("api 에러:::", err);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchBookInfo();
+  }, [isbn, navigate]);
+  return (
+    <div>
+      <h1>Book Review</h1>
+      <p>받아온 ISBN: {isbn}</p>
+    </div>
+  );
 };
 
 export default MyReviewPage;
