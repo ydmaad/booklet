428b4d8167baf08059778e2c6707bfb4673ca0fe
thwjd639 <thwjd639@gmail.com>
2025-10-16 22:06:48 +0900

    fix: 새로고침시 로그인 해제되는 오류 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
-import { Route, Routes } from "react-router-dom";
-import BarcodeScanPage from "./pages/BarcodeScanPage";
+import { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkSession } from './store/slices/authSlice';
+import type { AppDispatch, RootState } from './store/store';
+
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import MainPage from "./pages/MainPage";
@@ -10,21 +14,32 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ReviewDetail from "./components/ReviewDetail";
 import EditProfile from "./components/EditProfile";
+import ISBNScanner from "./pages/ISBNScanner";
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { user } = useSelector((state: RootState) => state.auth);
+
+  // 앱 시작 시 세션 확인 (새로고침 대응)
+  // Redux Persist가 이미 상태를 복원했지만,
+  // Supabase 세션이 유효한지 백그라운드에서 확인
+  useEffect(() => {
+    dispatch(checkSession());
+  }, [dispatch]);
+
   return (
     <div className="max-w-[1200px] mx-auto">
       <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage" element={user ? <MyPage /> : <Navigate to="/" />} />
         <Route path="/mypage/edit" element={<EditProfile />} />
-        <Route path="/barcode" element={<BarcodeScanPage />} />
+        <Route path="/barcode" element={<ISBNScanner />} />/
         <Route path="/isbn" element={<IsbnInputPage />} />
         <Route path="/my-review/:isbn" element={<ReviewCreatePage />} />
         <Route path="/review" element={<ReviewDetail />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <LoginPage />} />
+        <Route path="/register" element={user ? <Navigate to="/" /> : <RegisterPage />} />
       </Routes>
       <Footer />
     </div>
